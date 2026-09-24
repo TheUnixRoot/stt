6253466a564a8815ad7cabfa900865728f47b23d
Juan Pedro Dominguez Berdun <juanpe.domber@gmail.com>
2026-09-24 14:48:54 +0200

    Fix Android 14+ foreground service types (specialUse) and notification channels

--- app/src/main/java/com/theunixroot/stt/FloatingBubbleService.kt
+++ app/src/main/java/com/theunixroot/stt/FloatingBubbleService.kt
@@ -135,7 +135,13 @@ class FloatingBubbleService : Service(), LifecycleOwner, ViewModelStoreOwner, Sa
             setOngoing(true)
         }.build()
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
+            startForeground(
+                1001,
+                notification,
+                ServiceInfo.FOREGROUND_SERVICE_TYPE_MICROPHONE or ServiceInfo.FOREGROUND_SERVICE_TYPE_SPECIAL_USE
+            )
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
             startForeground(1001, notification, ServiceInfo.FOREGROUND_SERVICE_TYPE_MICROPHONE)
         } else {
             startForeground(1001, notification)
